47e7674b01a104685195159b2e40c2012534bad9
Jayesh0369 <prajapatijayesh6297@gmail.com>
2021-11-11 11:52:49 +0100

    add files

--- form-holder.php
+++ form-holder.php
@@ -15,11 +15,11 @@ $email_body = "User Name: $name. \n".
 
 $to = 'prajapatijayesh6297@gmail.com'
 
-$headers = "From: $email_from" \r\n;
+$headers = "From: $email_from \r\n";
 
 $headers .= "Reply-To: $visitor_email \r\n";
 
 mail($to,$email_subject,$email_body,$headers);
 
-header("Location:");
+header("Location: contact.hmtl");
 ?>
